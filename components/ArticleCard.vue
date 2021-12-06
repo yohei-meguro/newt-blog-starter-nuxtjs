3539011723fa65a21946eba3afb654f7741f6b15
atsushifujikawa <atsushi.fujikawa@newt.so>
2021-12-06 09:45:35 +0900

    fix: 不要なthisを削除

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -2,8 +2,8 @@
   <article v-if="article" class="Article">
     <NuxtLink :to="`/article/${article.slug}`" class="Article_Link">
       <div class="Article_Eyecatch">
-        <template v-if="this.article.coverImage">
-          <img :src="this.article.coverImage.src" alt="" />
+        <template v-if="article.coverImage">
+          <img :src="article.coverImage.src" alt="" />
         </template>
         <template v-else>
           <div class="Article_EyecatchEmpty">
@@ -16,8 +16,8 @@
         <div class="Article_Content">
           <div class="Article_Data">
             <div class="Article_Avatar">
-              <template v-if="this.article.author && this.article.author.profileImage">
-                <img :src="this.article.author.profileImage.src" alt="" width="32" height="32" />
+              <template v-if="article.author && article.author.profileImage">
+                <img :src="article.author.profileImage.src" alt="" width="32" height="32" />
               </template>
               <template v-else>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="#CCCCCC"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
