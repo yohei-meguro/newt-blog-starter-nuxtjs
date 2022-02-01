db07549c2e7b15f8f2fcef993bdc70e4531e78df
Kazuki Takahashi <kazuki.takahashi@newt.so>
2022-02-01 17:17:18 +0900

    fix: headerデザイン

--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,36 @@
 <template>
   <header class="Header">
-    <NuxtLink to="/" class="Title">
-      <span v-if="icon" class="Title_Icon">{{ icon }}</span>
-      <h1 v-if="useH1" class="Title_Text">{{ title }}</h1>
-      <div v-else class="Title_Text">{{ title }}</div>
-    </NuxtLink>
-    <div class="Search">
-      <button type="button" class="Search_Button" @click="focusInput">
-        <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M9.3890873 1.6109127c1.81744 1.81743998 2.0970461 4.59036739.8388184 6.7018035l3.3116969 3.3126728c.3547755.3547755.3257954.9589604-.0647289 1.3494847-.3626297.3626297-.9094871.4135198-1.2698126.1348865l-.0796721-.0701576-3.22015474-3.21985629C6.7465078 11.5258295 3.60410194 11.3822765 1.6109127 9.3890873c-2.1478836-2.14788361-2.1478836-5.63029099 0-7.7781746 2.14788361-2.1478836 5.63029099-2.1478836 7.7781746 0zM2.95984943 2.95984943c-1.40288642 1.40288642-1.40288642 3.67741472 0 5.08030114 1.40288642 1.40288642 3.67741472 1.40288642 5.08030114 0 1.40288642-1.40288642 1.40288642-3.67741472 0-5.08030114-1.40288642-1.40288642-3.67741472-1.40288642-5.08030114 0z"
-            fill="#333"
-            fill-rule="nonzero"
-          />
-        </svg>
-      </button>
-      <form action="/search">
-        <div class="Search_Input">
-          <input
-            ref="target"
-            v-model="searchText"
-            name="q"
-            type="search"
-            placeholder="Search"
-          />
-        </div>
-      </form>
+    <div class="Header_Inner">
+      <NuxtLink to="/" class="Title">
+        <span v-if="icon" class="Title_Icon">{{ icon }}</span>
+        <h1 v-if="useH1" class="Title_Text">{{ title }}</h1>
+        <div v-else class="Title_Text">{{ title }}</div>
+      </NuxtLink>
+      <div class="Link">
+        <a href="https://github.com/Newt-Inc/newt-blog-starter-nuxtjs" rel="noreferrer noopener" target="_blank">GitHub</a>
+      </div>
+      <div class="Search">
+        <button type="button" class="Search_Button" @click="focusInput">
+          <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M9.3890873 1.6109127c1.81744 1.81743998 2.0970461 4.59036739.8388184 6.7018035l3.3116969 3.3126728c.3547755.3547755.3257954.9589604-.0647289 1.3494847-.3626297.3626297-.9094871.4135198-1.2698126.1348865l-.0796721-.0701576-3.22015474-3.21985629C6.7465078 11.5258295 3.60410194 11.3822765 1.6109127 9.3890873c-2.1478836-2.14788361-2.1478836-5.63029099 0-7.7781746 2.14788361-2.1478836 5.63029099-2.1478836 7.7781746 0zM2.95984943 2.95984943c-1.40288642 1.40288642-1.40288642 3.67741472 0 5.08030114 1.40288642 1.40288642 3.67741472 1.40288642 5.08030114 0 1.40288642-1.40288642 1.40288642-3.67741472 0-5.08030114-1.40288642-1.40288642-3.67741472-1.40288642-5.08030114 0z"
+              fill="#333"
+              fill-rule="nonzero"
+            />
+          </svg>
+        </button>
+        <form action="/search">
+          <div class="Search_Input">
+            <input
+              ref="target"
+              v-model="searchText"
+              name="q"
+              type="search"
+              placeholder="Search"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   </header>
 </template>
@@ -61,17 +66,29 @@ export default {
 
 <style scoped>
 .Header {
-  display: flex;
   background: #fff;
-  padding: 8px;
+  padding: 8px 8px 8px 14px;
   position: sticky;
   top: 0;
   width: 100%;
   box-sizing: border-box;
   z-index: 10;
-  min-width: 0;
   flex-shrink: 0;
   box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1);
+}
+.Header_Inner {
+  min-width: 0;
+  display: flex;
+  max-width: 1024px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+@media (min-width: 600px) {
+  .Header {
+    padding: 8px 40px;
+  }
 }
 .Title {
   display: flex;
@@ -82,13 +99,14 @@ export default {
   text-decoration: none;
 }
 .Title_Icon {
-  width: 34px;
+  width: 20px;
   height: 34px;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 1.8rem;
   flex-shrink: 0;
+  margin: 0 8px -2px 0;
 }
 .Title_Icon img {
   width: 26px;
@@ -141,13 +159,13 @@ export default {
   border-radius: 4px;
   opacity: 0;
   position: absolute;
-  right: 8px;
-  top: 8px;
-  pointer-events: none;
+  right: 0;
+  top: 0;
+  z-index: -1;
 }
 .Search_Input input:focus {
   opacity: 1;
-  pointer-events: inherit;
+  z-index: 1;
 }
 .Search_Button {
   height: 34px;
@@ -171,5 +189,19 @@ export default {
   position: absolute;
   left: 10px;
   top: 10px;
+}
+.Link {
+  border-right: 1px solid #e5e5e5;
+  padding: 0 8px 0 0;
+  margin: 0 12px 0 0;
+}
+.Link > a {
+  color: #888;
+  text-decoration: none;
+  transition: color .2s;
+  padding: 3px 12px;
+}
+.Link > a:hover {
+  color: #333;
 }
 </style>
